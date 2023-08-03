1050e2f6df38ff4746d2156f6490ea88608d536d
Abood khashashneh <abdalrhmankhashashneh@gmail.com>
2023-08-03 09:22:26 +0300

    add convert page to image and pdf app

--- pdf_example_app/app/Http/Controllers/PDFcontroller.php
+++ pdf_example_app/app/Http/Controllers/PDFcontroller.php
@@ -2,19 +2,24 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use Spatie\Browsershot\Browsershot;
 use Illuminate\Support\Facades\Response;
-class PDFcontroller extends Controller
-{
-  function index(){
-    $html = view('welcome')->render();
 
-    $filePath =public_path('converted-image_'.date('ymd').'.png');
-    Browsershot::html($html)->save($filePath);
+class PDFController extends Controller
+{
+    public function index()
+    {
+        $html = view('welcome')->render();
+        $path = env('APP_URL') . '/codes_part_to_use_php/pdf_example_app/node_modules';
+        $filePath = public_path('converted-image_' . date('ymd') . '.pdf');
+
+        Browsershot::html($html)
+            ->setNodeBinary(env('NODE_BINARY', '/usr/local/bin/node')) // Use the correct Node.js binary path if required
+            ->setNpmBinary(env('NPM_BINARY', '/usr/local/bin/npm')) // Use the correct npm binary path if required
+            ->noSandbox()
+            ->save($filePath);
 
         return Response::download($filePath)->deleteFileAfterSend(true);
-    echo "Hi PDF" ;
-  }
+    }
 }
